9237c652f23d9404c9de876a538c03f622c753cb
Wissem Hammami <hammamiwissem21@gmail.com>
2023-03-03 10:15:18 -0800

    Update header

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://free-portfolio.web.app/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://free-portfolio.web.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
